e06a84f9f43a22d033804c3f7b58c5097ec90d1b
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-06-15 11:54:01 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button'
 
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
-     
+      <div style={{border: '1px solid black'}}>
+        <h2>Hello World</h2>
+        <Button variant="danger">Danger</Button>
+      </div>
     </div>
   );
 }
